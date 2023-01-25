934605d7052b82138f2c9f7e9cfe7aa66e971739
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-01-25 11:51:39 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<style>
+  :global(body) {
+    background: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+    height: 100vh;
+    color: white;
+    margin: 0;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  :global(*) {
+    box-sizing: border-box;
+  }
+</style>
+
+<slot></slot>
