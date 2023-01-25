4028423dae6b13362feba5c74384634a17140125
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-01-25 12:19:43 -0300

    Layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<style>
+  nav {
+    display: flex;
+    justify-content: center;
+    margin-top: 5rem;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 3rem;
+  }
+</style>
+
+<nav>
+  <a href="/classic">Classic</a>
+</nav>
