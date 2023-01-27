99617ec3e2313d9cb40f02a8096482b804811246
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-01-27 11:52:31 -0300

    Animate selector in

--- src/components/selector.svelte
+++ src/components/selector.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+  import { scale } from 'svelte/transition';
+
   import bgtriangle from '../images/bg-triangle.svg';
 	import { Item } from '../models';
 	import ItemIcon from './item-icon.svelte';
 
   export let onSelected: (item: Item) => void;
+  export let animate = true;
 
 </script>
 
-<section>
+<section in:scale={{ duration: animate ? 250 : 0 }}>
   <ItemIcon on:click={() => onSelected(Item.PAPER)} type={Item.PAPER} />
   <ItemIcon on:click={() => onSelected(Item.SCISSORS)} type={Item.SCISSORS} />
   <ItemIcon on:click={() => onSelected(Item.ROCK)} type={Item.ROCK} />
