5c405d7cf8ab141c1bfd6e6849ab93b3308446b0
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-01-30 12:48:37 -0300

    add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,38 @@
   import extendedRules from '../images/rules-extended.svg';
 </script>
 
-<nav>
-  <a href="/classic">
-    <img src={classicRules} alt="Classic Rules" />
-    Classic
-  </a>
-  <a href="/extended">
-    <img src={extendedRules} alt="Extended Rules" />
-    Extended
-  </a>
-</nav>
+<main>
+  <h1>Rock Paper Scissors</h1>
+
+  <nav>
+    <a href="/classic">
+      <img src={classicRules} alt="Classic Rules" />
+      Classic
+    </a>
+    <a href="/extended">
+      <img src={extendedRules} alt="Extended Rules" />
+      Extended
+    </a>
+  </nav>
+</main>
 
 <style>
+  h1 {
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: 0;
+  }
   nav {
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 10rem;
+    height: 100%;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
     height: 100vh;
   }
 
@@ -57,6 +72,30 @@
 
     img {
       max-height: 200px;
+    }
+
+    h1 {
+      font-size: 3rem;
+      padding-left: 0.5rem;
+      padding-right: 0.5rem;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    h1 {
+      font-size: 2rem;
+      padding: 5px;
+    }
+
+    nav {
+      margin-top: 2.5rem;
+      justify-content: start;
+      gap: 3rem;
+    }
+
+    a {
+      font-size: 1.8rem;
+      gap: 1rem;
     }
   }
 
