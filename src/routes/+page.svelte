7f6c43351273dd207dc72ecde3f5003be4444068
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-01-30 12:21:58 -0300

    Add rules to mode selector page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
+<script>
+  import classicRules from '../images/rules.svg';
+  import extendedRules from '../images/rules-extended.svg';
+</script>
+
 <nav>
-  <a href="/classic">Classic</a>
-  <a href="/extended">Extended</a>
+  <a href="/classic">
+    <img src={classicRules} alt="Classic Rules" />
+    Classic
+  </a>
+  <a href="/extended">
+    <img src={extendedRules} alt="Extended Rules" />
+    Extended
+  </a>
 </nav>
 
 <style>
@@ -8,13 +19,50 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-top: 5rem;
-    gap: 3rem;
+    gap: 10rem;
+    height: 100vh;
+  }
+
+  a:hover {
+    transform: scale(1.2);
+    color: white;
+  }
+
+  a:hover img {
+    opacity: 1;
   }
 
   a {
-    color: white;
+    color: rgb(219 219 219);
     text-decoration: none;
     font-size: 3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+    transition: all 0.25s;
+  }
+
+  img {
+    height: 300px;
+    opacity: 0.5;
+    transition: all 0.25s;
+  }
+
+  @media screen and (max-width: 1000px) {
+    nav {
+      flex-direction: column;
+      gap: 2rem;
+    }
+
+    img {
+      max-height: 200px;
+    }
+  }
+
+  @media (hover: none) {
+    img {
+      opacity: 1;
+    }
   }
 </style>
