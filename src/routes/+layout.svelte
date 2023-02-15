8ffb069749dea8275214ce1d957f62648af88403
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-02-15 12:16:53 -0300

    Add translations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,22 @@
+<script>
+  import { locale, Locale } from '$lib/i18n';
+
+  function changeLocale() {
+    locale.set($locale === Locale.es ? Locale.en : Locale.es);
+  }
+</script>
+
 <slot />
+
+<div class="lang-selector">
+  <button on:click|preventDefault={changeLocale}>
+    {#if $locale === Locale.es}
+      English
+    {:else}
+      Español
+    {/if}
+  </button>
+</div>
 
 <style>
   :global(html),
@@ -21,5 +39,24 @@
 
   :global(*) {
     box-sizing: border-box;
+  }
+
+  .lang-selector {
+    position: absolute;
+    top: 0.5rem;
+    right: 2rem;
+    display: flex;
+    gap: 1rem;
+    padding: 0.5rem;
+  }
+
+  .lang-selector button {
+    color: #a5a5a5;
+    text-decoration: none;
+    background: none;
+    outline: none;
+    border: none;
+    font-size: 1rem;
+    cursor: pointer;
   }
 </style>
