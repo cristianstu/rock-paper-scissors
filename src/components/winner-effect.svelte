16aa393824ef88671c19b0cc9248bb4fb1c7a74a
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-02-28 13:02:21 -0300

    refactor winner effect

--- src/components/winner-effect.svelte
+++ src/components/winner-effect.svelte
@@ -1,50 +1,36 @@
 <script>
   import { scale } from 'svelte/transition';
 
-  let size = '590px';
+  let size = '360px';
 </script>
 
-<div class="circle-1" style:--winner-effect-size={size} in:scale={{ duration: 250 }}>
-  <div class="circle-2">
-    <div class="circle-3" />
-  </div>
-</div>
+<div class="circle" style:--winner-effect-size={size} in:scale={{ duration: 250 }} />
 
 <style>
   div {
-    background-color: rgba(255, 255, 255, 0.02);
+    --circle-2: calc(var(--winner-effect-size) * 0.15);
+    --circle-3: calc(var(--winner-effect-size) * 0.30);
+
+    background-color: rgba(255, 255, 255, 0.025);
     border-radius: 50%;
     display: grid;
     place-content: center;
-  }
-
-  .circle-1 {
     width: var(--winner-effect-size);
     height: var(--winner-effect-size);
-    position: fixed;
-  }
-
-  .circle-2 {
-    width: calc(var(--winner-effect-size) * 0.78);
-    height: calc(var(--winner-effect-size) * 0.78);
-    z-index: 2;
-  }
-
-  .circle-3 {
-    width: calc(var(--winner-effect-size) * 0.61);
-    height: calc(var(--winner-effect-size) * 0.61);
-    z-index: 3;
+    position: absolute;
+    box-shadow: 0px 0px 0px var(--circle-2) rgb(255 255 255 / 2%),
+      0px 0px 0px var(--circle-3) rgb(255 255 255 / 2%);
   }
 
   @media screen and (max-width: 1000px) {
-    .circle-1 {
-      --winner-effect-size: 340px !important;
+    div {
+      --winner-effect-size: 220px !important;
     }
   }
 
   @media screen and (max-width: 550px) {
-    .circle-1 {
-      --winner-effect-size: 280px !important;
+    div {
+      --winner-effect-size: 180px !important;
     }
   }
 </style>
