b054357e2c5133181e96e49ea3d3d6d24ca3fd6a
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-03-01 17:17:54 -0300

    add url to share game

--- src/components/rock-paper-scissors.svelte
+++ src/components/rock-paper-scissors.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import Pusher from 'pusher-js';
+  import { onMount } from 'svelte';
+
   import type { Item } from '../models';
   import { getRandomItem } from '../lib/ramdom-item';
   import Header from './header.svelte';
@@ -8,18 +11,43 @@
   import { t } from '$lib/i18n';
 
   export let mode: GameMode = GameMode.CLASSIC;
+  export let remoteOponent = true;
+  export let roomCode: string = '';
 
   $: title = $t('app.title');
   let score = 0;
   let player1Choice: Item | null = null;
   let player2Choice: Item | null = null;
   let winner: Item | null = null;
+  let shareUrl = '';
+
+  onMount(() => {
+    if (remoteOponent && roomCode) {
+      Pusher.logToConsole = true;
+
+      const pusher = new Pusher('9e6d9731bfcb4a5657b3', {
+        cluster: 'us2'
+      });
+
+      const channel = pusher.subscribe(`room-${roomCode}`);
+      channel.bind('player-choice', function(data: any) {
+        alert(JSON.stringify(data));
+      });
+
+      return () => {
+        channel.unbind_all();
+        channel.unsubscribe();
+      };
+    }
+	});
 
   const handleSelected = (item: Item) => {
     player1Choice = item;
-    setTimeout(() => {
-      player2Choice = getRandomItem({ mode });
-    }, 3000);
+    if (!remoteOponent) {
+      setTimeout(() => {
+        player2Choice = getRandomItem({ mode });
+      }, 3000);
+    }
   };
 
   const playAgain = () => {
@@ -50,10 +78,28 @@
       }
     }
   }
+
+  $: if (globalThis.location && remoteOponent && roomCode) {
+    shareUrl = `${globalThis.location.protocol}//${globalThis.location.host}/classic/${roomCode}`;
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(shareUrl);
+  }
 </script>
 
 <main>
   <Header {title} {score} {mode} />
+
+  {#if roomCode}
+    <div class="share">
+      <h3>Share to play</h3>
+      <div>
+        <span title={shareUrl}>{shareUrl}</span>
+        <button on:click={copyToClipboard}>Copy</button>
+      </div>
+    </div>
+  {/if}
 
   {#if !player1Choice}
     <Selector {mode} onSelected={handleSelected} />
@@ -62,12 +108,61 @@
   {/if}
 </main>
 
-<style>
+<style lang="scss">
   main {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding: 3rem 2rem;
+  }
+
+  .share {
+    margin-top: 0rem;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+
+    span {
+      max-width: 60vw;
+      font-size: 1rem;
+      padding: 7px 20px;
+      background: red;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      background: rgb(0 0 0 / 20%);
+      border: rgb(255 255 255 / 12%);
+      border-radius: 7px 0px 0px 7px;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+      display: block;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    button {
+      height: 100%;
+      border: none;
+      padding: 0.5rem 1rem;
+      border-radius: 0 7px 7px 0;
+      text-transform: uppercase;
+      font-weight: bold;
+      font-size: 1rem;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      &:hover {
+        filter: brightness(0.9);
+      }
+      &:active {
+        filter: brightness(0.8);
+      }
+    }
   }
 </style>
---
+++ src/routes/classic/[code]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  import RockPaperScissors from '../../../components/rock-paper-scissors.svelte';
+  import { GameMode } from '../../../config';
+
+  const roomCode = $page.params.code;
+</script>
+
+<RockPaperScissors mode={GameMode.CLASSIC} {roomCode} />
