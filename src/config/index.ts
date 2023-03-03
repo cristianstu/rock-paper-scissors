0d74cc5baef58fe37e621b310b73066d5d6d056f
Cristian Stügelmayer <cristian@yellowspot.dev>
2023-03-03 16:06:19 -0300

    Use process.env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const PUSHER_APP_ID = import.meta.env.PUSHER_APP_ID;
-export const PUSHER_KEY = import.meta.env.PUSHER_KEY;
-export const PUSHER_SECRET = import.meta.env.PUSHER_SECRET;
-export const PUSHER_CLUSTER = import.meta.env.PUSHER_CLUSTER;
+export const PUSHER_APP_ID = process.env.PUSHER_APP_ID;
+export const PUSHER_KEY = process.env.PUSHER_KEY;
+export const PUSHER_SECRET = process.env.PUSHER_SECRET;
+export const PUSHER_CLUSTER = process.env.PUSHER_CLUSTER;
